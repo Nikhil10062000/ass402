17ba1920123d74971a9aa2ba2fd8f99c27939895
Nikhil10062000 <94104092+Nikhil10062000@users.noreply.github.com>
2021-12-30 12:21:36 +0530

    Add files via upload

---
+++ ass402.js
@@ -0,0 +1,8 @@
+//write a program to print all even numbers less than or equal to n (n is an input which we take using prompt)
+
+   var n = [];
+   var a = prompt("Enter the number");
+   n.push(a);  
+  for(  var i = 2 ; i <= n ; i = i + 2)
+  {  alert(i);
+   }
